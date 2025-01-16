ac8ed82d4e9c56451e8135f52424d3201d392185
Nili Scharf <nilischarf@gmail.com>
2025-01-16 09:52:24 +0200

    complete information flow lab

---
+++ src/components/Filter.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Filter({ onCategoryChange }) {
+    return (
+        <div className="Filter">
+            <select name="filter" onChange={onCategoryChange}>
+                <option value="All">Filter by Category</option>
+                <option value="Produce">Produce</option>
+                <option value="Dairy">Dairy</option>
+                <option value="Dessert">Dessert</option>
+            </select>
+        </div>
+    )
+}
+
+export default Filter
